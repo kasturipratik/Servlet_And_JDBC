caa98ec57e961cb14ee141e7866c9164813f7f8f
kasturipratik <pratik.parajuli@yahoo.com>
2018-08-14 16:50:48 -0400

    servlet example

---
+++ src/com/example/HrPage.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HrPage
+ */
+@WebServlet("/HrPage")
+public class HrPage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+  
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<HTML>");
+		out.println("<body> "
+				+ "<h2>Works</h2>"
+				+ "</body>"
+				+ "</html>");
+	}
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
